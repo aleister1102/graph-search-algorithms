0b599edbd2fcd74ab1d2d6ae21fba6b19db0f2ab
Lê Minh Quân <marucube35@gmail.com>
2023-03-22 15:57:52 +0700

    feat: add UCS

--- SearchAlgorithms.py
+++ SearchAlgorithms.py
@@ -1,6 +1,8 @@
 from Space import *
 from Constants import *
 import time
+import random
+
 
 def set_color(sc: pygame.Surface, node: Node, color):
     if color == grey:
@@ -14,17 +16,21 @@ def set_color(sc: pygame.Surface, node: Node, color):
     pygame.display.flip()
     time.sleep(0.02)
 
+
 def reconstruct_path(g: Graph, sc: pygame.Surface, father: list[int]):
     i = g.goal.value
     while i != -1:
-        if father[i] == -1: break
+        if father[i] == -1:
+            break
 
         curr, parent = g.grid_cells[i], g.grid_cells[father[i]]
-        curr_coordinate, parent_coordinate = (curr.x, curr.y), (parent.x, parent.y)
-        
+        curr_coordinate, parent_coordinate = (
+            curr.x, curr.y), (parent.x, parent.y)
+
         pygame.draw.line(sc, green, curr_coordinate, parent_coordinate, 2)
         set_color(sc, curr, grey)
         i = father[i]
+
 
 def DFS(g: Graph, sc: pygame.Surface):
     open_set: list[int] = [g.start.value]
@@ -81,6 +87,15 @@ def BFS(g: Graph, sc: pygame.Surface):
 
     reconstruct_path(g, sc, father)
 
+def get_min_node(open_set: dict[int]):
+    min = 100_000
+    min_node = -1
+    for i, v in open_set.items():
+        if v < min:
+            min = v
+            min_node = i
+    return min_node
+        
 
 def UCS(g: Graph, sc: pygame.Surface):
     open_set = {}
@@ -89,6 +104,35 @@ def UCS(g: Graph, sc: pygame.Surface):
     father = [-1]*g.get_len()
     cost = [100_000]*g.get_len()
     cost[g.start.value] = 0
+
+    while len(open_set):
+        min_node = get_min_node(open_set)
+        curr = g.grid_cells[min_node]
+        del open_set[min_node]
+        set_color(sc, curr, yellow)
+        
+        if g.is_goal(curr):
+            break
+        
+        if curr.value in closed_set:
+            continue
+        else:
+            closed_set.append(curr.value)
+            set_color(sc, curr, blue)
+        
+        for child in g.get_neighbors(curr):
+            if child.value not in closed_set and child.value not in open_set.keys():
+                # chi phí giữa các đỉnh là bao nhiêu?
+                cost[child.value] = cost[curr.value] + random.randint(1, 10)
+                open_set[child.value] = cost[child.value]
+                set_color(sc, child, red)
+                father[child.value] = curr.value
+            elif child.value in open_set.keys() and cost[child.value] < open_set[child.value]:
+                open_set[child.value] = cost[child.value]
+
+
+    reconstruct_path(g, sc, father)
+
 
 def AStar(g: Graph, sc: pygame.Surface):
     print('Implement A* algorithm')
